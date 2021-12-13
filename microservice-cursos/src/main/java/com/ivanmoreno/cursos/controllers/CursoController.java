81aed5c1fa073f64f6ec0348c9e88d8f0c818834
ivan <ivanmoreno405@gmail.com>
2021-12-13 11:19:44 -0600

    validation implemented

--- microservice-cursos/src/main/java/com/ivanmoreno/cursos/controllers/CursoController.java
+++ microservice-cursos/src/main/java/com/ivanmoreno/cursos/controllers/CursoController.java
@@ -3,8 +3,11 @@ package com.ivanmoreno.cursos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,7 +25,12 @@ import com.ivanmoreno.cursos.services.CursoService;
 public class CursoController extends CommonController<Curso, CursoService>{
 
 	@PutMapping("/{id}")
-	public ResponseEntity<?> edit(@RequestBody Curso curso, @PathVariable Long id) {
+	public ResponseEntity<?> edit(@Valid @RequestBody Curso curso, BindingResult result, @PathVariable Long id) {
+		
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
+		
 		Optional<Curso> cursoOpt = this.service.findById(id);
 		
 		if(!cursoOpt.isPresent()) {
--- microservice-cursos/src/main/java/com/ivanmoreno/cursos/models/entity/Curso.java
+++ microservice-cursos/src/main/java/com/ivanmoreno/cursos/models/entity/Curso.java
@@ -17,6 +17,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
 
 import com.ivanmoreno.commons.models.entity.Alumno;
 import com.ivanmoreno.commons.models.entity.Examen;
@@ -34,6 +35,7 @@ public class Curso {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotEmpty
 	private String nombre;
 	
 	@Column(name = "create_at")
